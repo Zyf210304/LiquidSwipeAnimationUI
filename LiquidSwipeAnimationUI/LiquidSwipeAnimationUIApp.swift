755a669e3c0914d9afbf5c0e399c6d44ed1b451a
张亚飞 <672233580@qq.com>
2021-06-27 22:36:28 +0800

    Initial Commit

---
+++ LiquidSwipeAnimationUI/LiquidSwipeAnimationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidSwipeAnimationUIApp.swift
+//  LiquidSwipeAnimationUI
+//
+//  Created by 张亚飞 on 2021/6/27.
+//
+
+import SwiftUI
+
+@main
+struct LiquidSwipeAnimationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
